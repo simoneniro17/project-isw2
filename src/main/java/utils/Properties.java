3bc0f1cc6230d78a702a2a2f831343c6b87c7273
Simone <simoneniro17@gmail.com>
2024-05-03 21:01:31 +0200

    Properties added

---
+++ src/main/java/utils/Properties.java
@@ -0,0 +1,22 @@
+package utils;
+
+//  Utility class for managing project properties
+public class Properties {
+    public static final String COMMON_PROJECT = "BOOKKEEPER";
+    public static final String CUSTOM_PROJECT = "STORM";
+    public static final String DATASET_DIRECTORY = "./dataset/";
+    public static final String DATASET_FILE = "dataset.csv";
+    public static final String FILE_EXTENSION = ".java";
+    
+    public static final String GIT_HUB_URL = "https://github.com/apache/";
+    
+    private Properties() {}
+    
+    /* public static void prova() {
+        System.out.println(GIT_HUB_URL + COMMON_PROJECT);
+    }
+    
+    public static void main(String[] args) {
+        prova();
+    } */
+}
